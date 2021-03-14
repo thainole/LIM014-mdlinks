0ee1d2f8a5a421824654cedb42bdca7cfb02609c
thainole <thainole@gmail.com>
2021-03-14 15:48:23 -0500

    funcion mdlinks hasta retornar array con md's

--- src/api.js
+++ src/api.js
@@ -1,38 +1,35 @@
 const path = require('path');
 const fs = require('fs');
-const Console = require('console');
+const console = require('console');
 
-// Para volver un path absoluto
+
+/* Para volver un path absoluto y validar la ruta */
 const resolvePath = (somePath) => (path.isAbsolute(somePath) ? somePath : path.resolve(somePath));
-
-// Para saber si la ruta existe / es correcta)
 const validPath = (paths) => fs.existsSync(paths);
 
-//Para obtener los MD files
-const elemArr = [];
+/* Para obtener los MD files */
+const elemArr = [];                                                 // Crear un array vacío para luego insertar los archivos md
 const getMdFiles = (paths) => {
-    const extName = path.extname(paths);
-    const pathName = path.basename(paths)
-    const infoPath = fs.statSync(paths);
+    const extName = path.extname(paths);                            // método para obtener la extensión (md)
+    const pathName = path.basename(paths)                           // retorna la última porción del path (para obtener el nombre)
+    const infoPath = fs.statSync(paths);                            // para acceder a la información de los archivos
 
-    if (infoPath.isDirectory() && pathName !== 'node_modules') {
-        const dirElem = fs.readdirSync(paths);  
-        if (dirElem.length > 0) {
-            dirElem.map((elem) => {
-                const newAbsPath = `${paths}/${elem}`;
-                return getMdFiles(newAbsPath);
+    if (infoPath.isDirectory() && pathName !== 'node_modules') {    // método que da true && el basename diferente a la carpeta node_modules
+        const dirElem = fs.readdirSync(paths);                      //Para leer el directorio, muestra sus elementos en un array
+        if (dirElem.length > 0) {                                   // Si hay más de un elemento...
+            dirElem.map((elem) => {                                 // recorrer todos los elementos del array
+                const newAbsPath = path.join(paths, elem);          // y unir el path + los elementos 
+                return getMdFiles(newAbsPath);                      // para luego aplicarles la misma función 
             }) 
         } else {
-            return 'This directory does not contain any file'
+            return 'This directory does not contain any file'       // en caso no haya archivos
         }
-    } else if (extName == '.md') {
-        elemArr.push(paths);
+    } else if (extName == '.md') {                                  // si la extensión es md
+        elemArr.push(paths);                                        // agregar ese path al array creado anteriormente 
     } 
-    return elemArr;
+    return elemArr;                                                 // retornar el array de los elementos con extensión md
 }    
-    
-console.log(getMdFiles('D:/Documentos/Laboratoria/Bootcamp'));
-//Duda, no puedo hacerlo con la diagonal invertida :c no sé porqué
+    //console.log(getMdFiles('D:/Carpeta'))
 
 module.exports = {
     resolvePath,
--- src/index.js
+++ src/index.js
@@ -3,16 +3,14 @@ const api = require('./api');
 // Debería ser (path,options)
 const mdLinks = (example) => {
     const absolutePath = api.resolvePath(example);
-    const pathIsValid = api.validPath(absolutePath);
+    const pathIsValid = api.validPath(absolutePath); 
 
     if (pathIsValid == true) {
-        api.getMdFiles(absolutePath);
+       return api.getMdFiles(absolutePath);
     }
 
-    //falta acomodar para que funcione
 }
 
-console.log(mdLinks('/Carpeta'));
-console.log(mdLinks('/Carpeta/Carpeta-1'));
+console.log(mdLinks('/Carpeta')); 
 
 module.exports = mdLinks;
