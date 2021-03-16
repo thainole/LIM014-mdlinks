d0558526dd9bb1e05757c4f0ad971eb2bb4f2dfd
thainole <thainole@gmail.com>
2021-03-15 22:08:04 -0500

    eliminación de comentarios extra

--- src/api.js
+++ src/api.js
@@ -6,24 +6,24 @@ const resolvePath = (somePath) => (path.isAbsolute(somePath) ? somePath : path.r
 const validPath = (paths) => fs.existsSync(paths);
 
 /* Para obtener los MD files */
-const elemArr = []; // Crear un array vacío para luego insertar los archivos md
+const elemArr = [];
 const getMdFiles = (paths) => {
-  const extName = path.extname(paths); // método para obtener la extensión (md)
-  const pathName = path.basename(paths); // retorna la última porción del path (obtener el nombre)
-  const infoPath = fs.statSync(paths); // para acceder a la información de los archivos
+  const extName = path.extname(paths);
+  const pathName = path.basename(paths);
+  const infoPath = fs.statSync(paths);
 
-  if (infoPath.isDirectory() && pathName !== 'node_modules') { // método que da true && el basename diferente a la carpeta node_modules
-    const dirElem = fs.readdirSync(paths); // Para leer directorio, muestra elementos en un array
-    if (dirElem.length > 0) { // Si hay más de un elemento...
-      dirElem.map((elem) => { // recorrer todos los elementos del array
-        const newAbsPath = path.join(paths, elem); // y unir el path + los elementos
-        return getMdFiles(newAbsPath); // para luego aplicarles la misma función
+  if (infoPath.isDirectory() && pathName !== 'node_modules') {
+    const dirElem = fs.readdirSync(paths);
+    if (dirElem.length > 0) {
+      dirElem.map((elem) => {
+        const newAbsPath = path.join(paths, elem);
+        return getMdFiles(newAbsPath);
       });
     }
-  } else if (extName === '.md') { // si la extensión es md
-    elemArr.push(paths); // agregar ese path al array creado anteriormente
+  } else if (extName === '.md') {
+    elemArr.push(paths);
   }
-  return elemArr; // retornar el array de los elementos con extensión md
+  return elemArr;
 };
 
 module.exports = {
