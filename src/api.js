8fafa00d2c6938576ce07821253f5597d18e097b
thainole <thainole@gmail.com>
2021-03-17 11:37:07 -0500

    complete until getting links

--- src/api.js
+++ src/api.js
@@ -1,16 +1,19 @@
 const path = require('path');
 const fs = require('fs');
 
-/* Para volver un path absoluto y validar la ruta */
-const resolvePath = (somePath) => (path.isAbsolute(somePath) ? somePath : path.resolve(somePath));
-const validPath = (paths) => fs.existsSync(paths);
+/* Función para volver un path absoluto y validar la ruta */
+const validPath = (paths) => {
+  const absPath = path.isAbsolute(paths) ? paths : path.resolve(paths);
+  const existPath = fs.existsSync(absPath);
+  return existPath;
+};
 
-/* Para obtener los MD files */
-const elemArr = [];
+/* Función para obtener los MD files */
 const getMdFiles = (paths) => {
   const extName = path.extname(paths);
   const pathName = path.basename(paths);
   const infoPath = fs.statSync(paths);
+  const elemArr = [];
 
   if (infoPath.isDirectory() && pathName !== 'node_modules') {
     const dirElem = fs.readdirSync(paths);
@@ -26,40 +29,36 @@ const getMdFiles = (paths) => {
   return elemArr;
 };
 
-// eslint-disable-next-line no-useless-escape
 const regx = /\[([\w\s\d.()]+)\]\(((?:\/|https?:\/\/)[\w\d./?=#&_%~,.:-]+)\)/mg;
 const regxLink = /\(((?:\/|https?:\/\/)[\w\d./?=#&_%~,.:-]+)\)/mg;
 const regxText = /\[([\w\s\d.()]+)\]/g;
 
-/* Funciones para leer archivos y extraer links */
-const readFile = (file) => (fs.readFileSync(file, 'utf-8'));
-const matchLinks = (file) => (file.match(regx));
-
-const getLinks = (mypath) => {
+/* Función para leer archivos y extraer links */
+const getLinks = (paths) => {
   const linksArr = [];
-  const getMdFilesArr = getMdFiles(mypath);
-  getMdFilesArr.map((myfile) => {
-    const fileRead = readFile(myfile);
-    const links = matchLinks(fileRead);
+  const mdfilesArr = getMdFiles(paths);
+  mdfilesArr.forEach((myfile) => {
+    const fileRead = fs.readFileSync(myfile, 'utf-8');
+    const links = fileRead.match(regx);
     if (links) {
-      links.map((link) => {
+      links.forEach((link) => {
         const myhref = link.match(regxLink).join().slice(1, -1); // con join vuelvo string mi array
         const mytext = link.match(regxText).join().slice(1, -1); // con el slice corto () []
-        const arrLinksObj = {
+        const linksObj = {
           href: myhref,
           text: mytext,
           file: myfile,
         };
-        linksArr.push(arrLinksObj);
+        linksArr.push(linksObj);
       });
     }
   });
   return linksArr;
 };
+
 console.log(getLinks('D:\\Documentos\\Laboratoria\\Bootcamp\\LIM014-mdlinks\\README.md'));
 
 module.exports = {
-  resolvePath,
   validPath,
   getMdFiles,
   getLinks,
