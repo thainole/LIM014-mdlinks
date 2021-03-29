77ce101270c11c68f5040d9fceb9ca3ca2a4874f
thainole <thainole@gmail.com>
2021-03-29 18:27:22 -0500

    iniciando el cli

--- src/cli.js
+++ src/cli.js
@@ -1,1 +1,10 @@
-const api = require('./api');
+#!/usr/bin/env node
+
+// Grab provided args
+const [,, ...args] = process.argv;
+
+// Print hello world provided args
+console.log(`Hello World ${args}`);
+
+// Se ve como consola en ./src/cli.js
+// Debería crear otro módulo para poner ya todo ? o solo en este
